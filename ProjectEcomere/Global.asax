0ea5847d1d479bd2c2671553febe96205a717182
datnmse <nguyen.manhdat1309@gmail.com>
2016-09-27 01:01:00 +0700

    first commit

---
+++ ProjectEcomere/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectEcomere.MvcApplication" Language="C#" %>
---
+++ ProjectEcomere/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectEcomere.Startup))]
+namespace ProjectEcomere
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
